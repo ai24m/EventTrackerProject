f7232dd7ae69fb418ad54ee190256ac5f49acb04
ai24m <powellallysa@gmail.com>
2022-01-07 15:40:49 -0700

    boot project setup done

---
+++ MorePractice/src/main/java/com/skilldistillery/algorithmpractice/MorePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.algorithmpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorePracticeApplication.class, args);
+	}
+
+}
