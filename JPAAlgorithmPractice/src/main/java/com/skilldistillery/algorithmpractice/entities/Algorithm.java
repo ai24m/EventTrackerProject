673ab96e2a12c3a87740b2a26ff648111477cb26
ai24m <powellallysa@gmail.com>
2022-01-07 15:34:12 -0700

    jdbc connection

---
+++ JPAAlgorithmPractice/src/main/java/com/skilldistillery/algorithmpractice/entities/Algorithm.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.algorithmpractice.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
+public class Algorithm {
+
+//	fields 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	
+	private String title; 
+	
+	private String description; 
+	
+	private int rating;
+	
+//  getters and setters 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	} 
+	
+//	toString
+	
+}
